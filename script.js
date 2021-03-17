7961e6c273670023ea8a0ab10a11df15f18c7373
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-17 13:30:04 +0000

    5-03

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('b1').addEventListener('click', change1)
 
 function change1 () {
-	document.getElementById('paragraph').innerHTML = document.getElementById('text-box').value
+  document.getElementById('paragraph').innerHTML = document.getElementById('text-box').value
 }
