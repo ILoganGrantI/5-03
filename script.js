cdf6e5dac9b9cc20d05a555a096387313d78656e
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-17 13:14:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('b1').addEventListener('click', change1)
+
+function change1 () {
+	document.getElementById('paragraph').innerHTML = document.getElementById('text-box').value
+}
